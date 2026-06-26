5d54423d05497fe3fb96880ad93640924dd21e02
jmayta1984 <jmayta@pucp.edu.pe>
2026-06-26 16:34:10 -0500

    feat: implement employee management in main application

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<Employee> employees = new ArrayList<>();
+
+        employees.add(new AdministrativeEmployee("A001", "John Doe", 3000, 1000));
+
+        employees.add(new SalesEmployee("S001", "Jane Smith", 2000, 500));
+
+        employees.add(new SupportEmployee("P001", "Bob Johnson", 2500, 10));
+
+        for (Employee employee : employees) {
+            employee.showInformation();
+        }
     }
 }
